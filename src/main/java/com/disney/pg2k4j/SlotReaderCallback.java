04e303b4cf29047c722fa6da8f2a0452525afa81
Ryan Kass <ryan.kass@bamtechmedia.com>
2018-08-09 14:36:37 -0400

    louder failure logging (#4)

--- src/main/java/com/disney/pg2k4j/SlotReaderCallback.java
+++ src/main/java/com/disney/pg2k4j/SlotReaderCallback.java
@@ -1,6 +1,9 @@
 package com.disney.pg2k4j;
 
+import com.amazonaws.services.kinesis.producer.Attempt;
+import com.amazonaws.services.kinesis.producer.UserRecordFailedException;
 import com.amazonaws.services.kinesis.producer.UserRecordResult;
+import com.google.common.collect.Iterables;
 import com.google.common.util.concurrent.FutureCallback;
 import org.postgresql.replication.LogSequenceNumber;
 import org.slf4j.Logger;
@@ -22,7 +25,11 @@ public class SlotReaderCallback implements FutureCallback<UserRecordResult> {
 
     @Override
     public void onFailure(Throwable t) {
-        logger.trace("Failed to put record with postgres sequence number {} onto the stream", lsn, t);
+        logger.error("Failed to put record with postgres sequence number {} onto the stream{}", lsn, t);
+        if (t instanceof UserRecordFailedException) {
+            final Attempt last = Iterables.getLast(((UserRecordFailedException) t).getResult().getAttempts());
+            logger.error("Failed to put record. Error code '{}' : '{}'.", last.getErrorCode(), last.getErrorMessage());
+        }
     }
 
     @Override
